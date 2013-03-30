ef64a0d8e7f8ae5d9e134f8931a0e0e1c6060570
zhuyouming <zhuyouming1988@gmail.com>
2013-03-30 23:17:26 +0800

    add all

--- 5TcpExample/echoclient.c
+++ 5TcpExample/echoclient.c
@@ -1,2 +1,50 @@
-:q
+#include "../head.h"
 
+
+int main(void)
+{
+    int ret = 0;
+    int connfd = 0;
+    char buff[BUFFSIZE_MY];
+    int count = 0;
+    char c;
+    
+    struct sockaddr_in servaddr;
+    
+    bzero(&servaddr, sizeof(servaddr));
+    
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(7788);
+    if(inet_pton(AF_INET, "192.168.1.66", &servaddr) <= 0)
+        printf("inet_pton error\n");
+    
+    connfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(connfd <= 0)
+        printf("socket error\n");
+    
+    ret = connect(connfd, (SA *)&servaddr, sizeof(servaddr));
+    if(ret > 0)
+    for( ; ; )
+    {
+        count = 0;
+        bzero(buff, strlen(buff));
+        while((c = getchar()) != EOF)
+        {
+            buff[count++] = c;
+            if('\n' == c)
+            {
+                buff[count++] = 0;//NULL
+            }
+        }
+        if(count != write(connfd, buff))
+            printf("write error\n");
+        bzero(buff, strlen(buff));
+        while(read(connfd, buff, BUFFSIZE_MY) > 0)
+            printf("receive:\n%s\n", buff);
+    }
+    else
+    {
+        printf("connect failed\n");
+    }
+    return 0;
+}
--- head.h
+++ head.h
@@ -1,3 +1,10 @@
-#inlcude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <netinet/in.h>
+
+
+#define SA struct sockaddr
+
+
+#define BUFFSIZE_MY 1024
